95ae8854baa9354647884ccc59d6513c39800e01
Szymon Walter <waltersz@protonmail.com>
2021-01-23 17:06:16 +0100

    Add "auto-fixing" of operands

--- instf.c
+++ instf.c
@@ -1,6 +1,369 @@
 #include <stdarg.h>
 #include "include/openasm.h"
 
+static size_t openasm_opsize(int tag) {
+    switch (tag) {
+    case OPENASM_OP_AL:
+    case OPENASM_OP_REG8:
+    case OPENASM_OP_IMM8:
+    case OPENASM_OP_MEMORY8:
+        return 8;
+    case OPENASM_OP_AX:
+    case OPENASM_OP_REG16:
+    case OPENASM_OP_IMM16:
+    case OPENASM_OP_MEMORY16:
+        return 16;
+    case OPENASM_OP_EAX:
+    case OPENASM_OP_REG32:
+    case OPENASM_OP_IMM32:
+    case OPENASM_OP_MEMORY32:
+        return 32;
+    case OPENASM_OP_RAX:
+    case OPENASM_OP_REG64:
+    case OPENASM_OP_IMM64:
+    case OPENASM_OP_MEMORY64:
+        return 64;
+    default:
+        abort();
+    }
+}
+
+static const char *openasm_match_reg(const char *op, size_t size) {
+    if (op[0] == 'r') {
+        ++op;
+    } else if (op[0] == 'e') {
+        ++op;
+    }
+
+    switch (op[0]) {
+    case 'a':
+        switch (size) {
+        case 8:
+            return "al";
+        case 16:
+            return "ax";
+        case 32:
+            return "eax";
+        case 64:
+            return "rax";
+        default:
+            abort();
+        }
+    case 'c':
+        switch (size) {
+        case 8:
+            return "cl";
+        case 16:
+            return "cx";
+        case 32:
+            return "ecx";
+        case 64:
+            return "rcx";
+        default:
+            abort();
+        }
+    case 'd':
+        switch (op[1]) {
+        case 'i':
+            switch (size) {
+            case 8:
+                return "dil";
+            case 16:
+                return "di";
+            case 32:
+                return "edi";
+            case 64:
+                return "rdi";
+            default:
+                abort();
+            }
+        default:
+            switch (size) {
+            case 8:
+                return "dl";
+            case 16:
+                return "dx";
+            case 32:
+                return "edx";
+            case 64:
+                return "rdx";
+            default:
+                abort();
+            }
+        }
+    case 'b':
+        switch (op[1]) {
+        case 'p':
+            switch (size) {
+            case 8:
+                return "bpl";
+            case 16:
+                return "bp";
+            case 32:
+                return "ebp";
+            case 64:
+                return "rbp";
+            default:
+                abort();
+            }
+        default:
+            switch (size) {
+            case 8:
+                return "bl";
+            case 16:
+                return "bx";
+            case 32:
+                return "ebx";
+            case 64:
+                return "rbx";
+            default:
+                abort();
+            }
+        }
+    case 's':
+        switch (op[1]) {
+        case 'p':
+            switch (size) {
+            case 8:
+                return "spl";
+            case 16:
+                return "sp";
+            case 32:
+                return "esp";
+            case 64:
+                return "rsp";
+            default:
+                abort();
+            }
+        case 'i':
+            switch (size) {
+            case 8:
+                return "sil";
+            case 16:
+                return "si";
+            case 32:
+                return "esi";
+            case 64:
+                return "rsi";
+            default:
+                abort();
+            }
+        default:
+            abort();
+        }
+    case '8':
+        switch (size) {
+        case 8:
+            return "r8b";
+        case 16:
+            return "r8w";
+        case 32:
+            return "r8d";
+        case 64:
+            return "r8";
+        default:
+            abort();
+        }
+    case '9':
+        switch (size) {
+        case 8:
+            return "r9b";
+        case 16:
+            return "r9w";
+        case 32:
+            return "r9d";
+        case 64:
+            return "r9";
+        default:
+            abort();
+        }
+    case '1':
+        switch (op[1]) {
+        case '0':
+            switch (size) {
+            case 8:
+                return "r10b";
+            case 16:
+                return "r10w";
+            case 32:
+                return "r10d";
+            case 64:
+                return "r10";
+            default:
+                abort();
+            }
+        case '1':
+            switch (size) {
+            case 8:
+                return "r11b";
+            case 16:
+                return "r11w";
+            case 32:
+                return "r11d";
+            case 64:
+                return "r11";
+            default:
+                abort();
+            }
+        case '2':
+            switch (size) {
+            case 8:
+                return "r12b";
+            case 16:
+                return "r12w";
+            case 32:
+                return "r12d";
+            case 64:
+                return "r12";
+            default:
+                abort();
+            }
+        case '3':
+            switch (size) {
+            case 8:
+                return "r13b";
+            case 16:
+                return "r13w";
+            case 32:
+                return "r13d";
+            case 64:
+                return "r13";
+            default:
+                abort();
+            }
+        case '4':
+            switch (size) {
+            case 8:
+                return "r14b";
+            case 16:
+                return "r14w";
+            case 32:
+                return "r14d";
+            case 64:
+                return "r14";
+            default:
+                abort();
+            }
+        case '5':
+            switch (size) {
+            case 8:
+                return "r15b";
+            case 16:
+                return "r15w";
+            case 32:
+                return "r15d";
+            case 64:
+                return "r15";
+            default:
+                abort();
+            }
+        default:
+            abort();
+        }
+    default:
+        abort();
+    }
+}
+
+static int openasm_match_opsize(OpenasmOperand *op, int tag) {
+    switch (openasm_opsize(tag)) {
+    case 8:
+        switch (op->tag) {
+        case OPENASM_OP_REG8:
+        case OPENASM_OP_REG16:
+        case OPENASM_OP_REG32:
+        case OPENASM_OP_REG64:
+            op->reg = openasm_match_reg(op->reg, 8);
+            return OPENASM_OP_REG8;
+        case OPENASM_OP_IMM8:
+        case OPENASM_OP_IMM16:
+        case OPENASM_OP_IMM32:
+        case OPENASM_OP_IMM64:
+            return OPENASM_OP_IMM8;
+        case OPENASM_OP_MEMORY8:
+        case OPENASM_OP_MEMORY16:
+        case OPENASM_OP_MEMORY32:
+        case OPENASM_OP_MEMORY64:
+            if (op->mem.base) op->mem.base = openasm_match_reg(op->mem.base, 8);
+            if (op->mem.index) op->mem.index = openasm_match_reg(op->mem.index, 8);
+            return OPENASM_OP_MEMORY8;
+        default:
+            abort();
+        }
+    case 16:
+        switch (op->tag) {
+        case OPENASM_OP_REG8:
+        case OPENASM_OP_REG16:
+        case OPENASM_OP_REG32:
+        case OPENASM_OP_REG64:
+            op->reg = openasm_match_reg(op->reg, 16);
+            return OPENASM_OP_REG16;
+        case OPENASM_OP_IMM8:
+        case OPENASM_OP_IMM16:
+        case OPENASM_OP_IMM32:
+        case OPENASM_OP_IMM64:
+            return OPENASM_OP_IMM16;
+        case OPENASM_OP_MEMORY8:
+        case OPENASM_OP_MEMORY16:
+        case OPENASM_OP_MEMORY32:
+        case OPENASM_OP_MEMORY64:
+            if (op->mem.base) op->mem.base = openasm_match_reg(op->mem.base, 16);
+            if (op->mem.index) op->mem.index = openasm_match_reg(op->mem.index, 16);
+            return OPENASM_OP_MEMORY16;
+        default:
+            abort();
+        }
+    case 32:
+        switch (op->tag) {
+        case OPENASM_OP_REG8:
+        case OPENASM_OP_REG16:
+        case OPENASM_OP_REG32:
+        case OPENASM_OP_REG64:
+            op->reg = openasm_match_reg(op->reg, 32);
+            return OPENASM_OP_REG32;
+        case OPENASM_OP_IMM8:
+        case OPENASM_OP_IMM16:
+        case OPENASM_OP_IMM32:
+        case OPENASM_OP_IMM64:
+            op->reg = openasm_match_reg(op->reg, 64);
+            return OPENASM_OP_IMM32;
+        case OPENASM_OP_MEMORY8:
+        case OPENASM_OP_MEMORY16:
+        case OPENASM_OP_MEMORY32:
+        case OPENASM_OP_MEMORY64:
+            if (op->mem.base) op->mem.base = openasm_match_reg(op->mem.base, 32);
+            if (op->mem.index) op->mem.index = openasm_match_reg(op->mem.index, 32);
+            return OPENASM_OP_MEMORY32;
+        default:
+            abort();
+        }
+    case 64:
+        switch (op->tag) {
+        case OPENASM_OP_REG8:
+        case OPENASM_OP_REG16:
+        case OPENASM_OP_REG32:
+        case OPENASM_OP_REG64:
+            return OPENASM_OP_REG64;
+        case OPENASM_OP_IMM8:
+        case OPENASM_OP_IMM16:
+        case OPENASM_OP_IMM32:
+        case OPENASM_OP_IMM64:
+            return OPENASM_OP_IMM64;
+        case OPENASM_OP_MEMORY8:
+        case OPENASM_OP_MEMORY16:
+        case OPENASM_OP_MEMORY32:
+        case OPENASM_OP_MEMORY64:
+            if (op->mem.base) op->mem.base = openasm_match_reg(op->mem.base, 64);
+            if (op->mem.index) op->mem.index = openasm_match_reg(op->mem.index, 64);
+            return OPENASM_OP_MEMORY64;
+        default:
+            abort();
+        }
+    default:
+        abort();
+    }
+}
+
 int openasm_instf(OpenasmBuffer *buf, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -113,7 +476,7 @@ int openasm_instf(OpenasmBuffer *buf, const char *fmt, ...) {
                 } break;
                 case 'm': {
                     struct OpenasmMemory mem = va_arg(args, struct OpenasmMemory);
-                    operands[arity++].mem = mem;
+                    operands[arity].mem = mem;
                     if (fmt[1] == '8') {
                         ++fmt;
                         operands[arity++].tag = OPENASM_OP_MEMORY8;
@@ -253,12 +616,31 @@ int openasm_instf(OpenasmBuffer *buf, const char *fmt, ...) {
     } else {
         tag = OPENASM_CONS2(operands[1].tag, operands[0].tag);
     }
+
     for (struct OpenasmEntry *entry = openasm_inst; entry->mnem; entry++) {
         if (strcmp(mnemonic, entry->mnem) == 0) {
             int (*fn)(OpenasmBuffer *, OpenasmOperand *) = entry->inst_table[tag];
             if (!fn) {
-                fprintf(stderr, "error: invalid combination of opcode and operands: \"%s\"\n", mnemonic);
-                return 1;
+                // second attempt, change operand size
+                size_t size0 = openasm_opsize(operands[0].tag);
+                size_t size1 = openasm_opsize(operands[1].tag);
+                if (size0 > size1) {
+                    operands[0].tag = openasm_match_opsize(&operands[0], operands[1].tag);
+                    fprintf(stderr, "warning: truncating 1st operand to match 2nd operand's size\n");
+                } else if (size1 > size0) {
+                    operands[1].tag = openasm_match_opsize(&operands[1], operands[0].tag);
+                    fprintf(stderr, "warning: truncating 2nd operand to match 1st operand's size\n");
+                }
+                tag = OPENASM_CONS2(operands[1].tag, operands[0].tag);
+                fn = entry->inst_table[tag];
+                if (!fn) {
+                    if (arity == 1) {
+                        fprintf(stderr, "error: invalid combination of opcode and operands: \"%s %d\"\n", mnemonic, operands[0].tag);
+                    } else {
+                        fprintf(stderr, "error: invalid combination of opcode and operands: \"%s %d, %d\"\n", mnemonic, operands[0].tag, operands[1].tag);
+                    }
+                    return 1;
+                }
             }
             return fn(buf, operands);
         }
